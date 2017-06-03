1a3a283f86551619abbc9eef178617f2edb93362
Moovendra Dinesh Babu Ganesan <moovendrag@znodchn.com>
2017-06-03 19:14:35 +0530

    Znod app site angular setup initation start

--- znodapp/src/app/app.module.ts
+++ znodapp/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
