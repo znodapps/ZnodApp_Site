c115b536fd1a7efcad6f98aae134f3c81b96de74
Moovendra Dinesh Babu Ganesan <moovendrag@znodchn.com>
2017-06-03 19:48:04 +0530

    updated with sample navigation module and component

--- znodapp/src/app/app.module.ts
+++ znodapp/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu.component';
+import { WeatherComponent } from './weather/weather.component';
+import { CurrencyComponent } from './currency/currency.component';
+import { MovieComponent } from './movie/movie.component';
+import { CONST_ROUTING } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    WeatherComponent,
+    CurrencyComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule    
+    HttpModule,
+    CONST_ROUTING    
   ],
   providers: [],
   bootstrap: [AppComponent]
