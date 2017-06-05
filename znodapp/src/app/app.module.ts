5d2823b078327a1f6fb10dec4e27468d93f4b1df
Narayanan SELVARAJ <narayanans@intentsg.com>
2017-06-05 13:11:39 +0530

    worked with menu

--- znodapp/src/app/app.module.ts
+++ znodapp/src/app/app.module.ts
@@ -2,21 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu.component';
-import { WeatherComponent } from './weather/weather.component';
-import { CurrencyComponent } from './currency/currency.component';
-import { MovieComponent } from './movie/movie.component';
 import { CONST_ROUTING } from './app.routing';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    WeatherComponent,
-    CurrencyComponent,
-    MovieComponent
+    HomeComponent,
+    ProductComponent,
+    FeaturesComponent,
+    PricingComponent,
+    CustomersComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- znodapp/src/app/app.routing.ts
+++ znodapp/src/app/app.routing.ts
@@ -1,12 +1,18 @@
 import { Routes, RouterModule } from '@angular/router';
-import { CurrencyComponent } from "./currency/currency.component";
-import { WeatherComponent } from "./weather/weather.component";
-import { MovieComponent } from "./movie/movie.component";
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ContactComponent } from './contact/contact.component';
 const MAINMENU_ROUTES: Routes = [
     //full : makes sure the path is absolute path
     { path: '', redirectTo: '/weather', pathMatch: 'full' },
-    { path: 'weather', component: WeatherComponent },
-    { path: 'movie', component: MovieComponent },
-    { path: 'currency', component: CurrencyComponent } 
+    { path: 'home', component: HomeComponent }, 
+    { path: 'product', component: ProductComponent }, 
+    { path: 'features', component: FeaturesComponent }, 
+    { path: 'pricing', component: PricingComponent }, 
+    { path: 'customers', component: CustomersComponent }, 
+    { path: 'contact', component: ContactComponent } 
 ];
 export const CONST_ROUTING = RouterModule.forRoot(MAINMENU_ROUTES);
